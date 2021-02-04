7700c9378442f948a5c7dfcc75dca46000dd4fec
Renan Paganini <renanpa1996@gmail.com>
2021-02-04 19:40:07 -0300

    Criação dos cactos e sua funcionalidades

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 let isJumping = false;
 
 function handleKeyUp(event) {
@@ -34,4 +35,27 @@ function jump() {
     }, 20);
 }
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
