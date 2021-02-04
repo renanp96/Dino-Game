9fde08056334aa1cdd58a03f6e58cfcffdbfe270
Renan Paganini <renanpa1996@gmail.com>
2021-02-04 19:26:38 -0300

    Funções do dinossauro finalizadas

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 const dino = document.querySelector('.dino');
+let isJumping = false;
 
-document.addEventListener('keyup');
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if (!isJumping) {
+            jump();
+        }
+    }
+}
+
+function jump() {
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            //Descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            //Subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+document.addEventListener('keyup', handleKeyUp);
