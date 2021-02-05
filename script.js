6d996fd554cb8b89142649ef4682d9f9a7023b39
Renan Paganini <renanpa1996@gmail.com>
2021-02-05 09:41:19 -0300

    Função de game over finalizado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -11,8 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
-
     let upInterval = setInterval(() => {
         if (position >= 150) {
             clearInterval(upInterval);
@@ -48,6 +47,10 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //Game Over!
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim do Jogo</h1>'
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
